e44f7345282601fd0f2ca70613e6d05f7e088e71
Oluwatomi Duyile <thecodingpoet@gmail.com>
2019-03-05 07:55:16 +0100

    Implement main function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Weather Application");
+import  { getWeatherInCelcius, getWeatherInFahrenheit } from './modules/weather';
+import { getTime } from './modules/time';
+
+const main = () => {
+  const locations = ['lagos', 'New York', 'Pittsburgh', 'Tokyo', 'Berlin', 'Ontario'];
+  console.log('Fetching...\n');
+  locations.forEach(async (location) => {
+    const weatherInCelcius = await getWeatherInCelcius(location);
+    const weatherInFahrenheit = await getWeatherInFahrenheit(location);
+    const time = await getTime(location);
+    console.log(`The temperature at ${location} is ${weatherInCelcius} celcius and ${weatherInFahrenheit} fahrenheit`)
+    console.log(`The time is ${time} \n`)
+  });
+}
+
+main();
